44fc6202b2fb722a4f703e187e75a4ad3afd1986
ronniej <ronniej@naver.com>
2025-01-14 10:06:34 +0900

    화면 크기 추가 & 화면 지우기 기능 업데이트

--- Engine/Engine/Engine.cpp
+++ Engine/Engine/Engine.cpp
@@ -11,13 +11,42 @@
 Engine* Engine::instance = nullptr;
 
 Engine::Engine()
-	: quit(false), mainLevel(nullptr)
+	: quit(false), mainLevel(nullptr), screenSize(40, 25)
 {
 	// 싱글톤 객체 설정.
 	instance = this;
 
 	// 기본 타겟 프레임 속도 설정.
 	SetTargetFrameRate(60.0f);
+
+	// 화면 지울 때 사용할 버퍼 초기화.
+	// 1. 버퍼 크기 할당.
+	emptyStringBuffer = new char[(screenSize.x + 1) * screenSize.y + 1];
+
+	// 버퍼 덮어쓰기.
+	memset(emptyStringBuffer, ' ', (screenSize.x + 1) * screenSize.y + 1);
+
+	// 2. 값 할당.
+	for (int y = 0; y < screenSize.y; ++y)
+	{
+		//for (int x = 0; x < screenSize.x; ++x)
+		//{
+		//	// 인덱스 계산 잘하기.
+		//	emptyStringBuffer[(y * (screenSize.x + 1)) + x] = ' ';
+		//}
+
+		// 각 줄 끝에 개행 문자 추가.
+		emptyStringBuffer[(y * (screenSize.x + 1)) + screenSize.x] = '\n';
+	}
+
+	// 마지막에 널 문자 추가.
+	emptyStringBuffer[(screenSize.x + 1) * screenSize.y] = '\0';
+
+	// 디버깅.
+#if _DEBUG
+	char buffer[2048];
+	strcpy_s(buffer, 2048, emptyStringBuffer);
+#endif
 }
 
 Engine::~Engine()
@@ -83,7 +112,7 @@ void Engine::Run()
 		{
 			// 입력 처리 (현재 키의 눌림 상태 확인).
 			ProcessInput();
-			
+
 			// 업데이트 가능한 상태에서만 프레임 업데이트 처리.
 			if (shouldUpdate)
 			{
@@ -241,13 +270,15 @@ void Engine::Clear()
 {
 	// 화면의 (0,0)으로 이동.
 	SetCursorPosition(0, 0);
-	
+
 	// 화면 지우기.
-	int height = 25;
-	for (int ix = 0; ix < height; ++ix)
-	{
-		std::cout << "                               \n";
-	}
+	std::cout << emptyStringBuffer;
+
+	//int height = 25;
+	//for (int ix = 0; ix < height; ++ix)
+	//{
+	//	std::cout << "                               \n";
+	//}
 
 	// 화면의 (0,0)으로 이동.
 	SetCursorPosition(0, 0);
